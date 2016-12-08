5658f1d577aedfcc772886c3e914c3635cb85ce4
GBasanta11 <gbasanta37@gmail.com>
2016-12-08 09:55:08 +0900

    Aloha.java Echo.java added

---
+++ 20161208/Echo.java
@@ -0,0 +1,14 @@
+public class Echo{
+	public static void main(String[] args) {
+		//System.out.println(args[0]);
+		
+		//System.out.println(args[0] +"さん、アロハ");
+	String data = new java.util.Scanner(System.in).nextLine();
+	System.out.println(data);
+
+	if(data.equals("Aoki")) {
+		break;
+	}
+	}
+
+}

776ed35e113d416ded26bbd19a2bcb45dfaac872
GBasanta11 <gbasanta37@gmail.com>
2016-12-13 10:07:46 +0900

    exit をつくりました

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,9 +1,12 @@
 public class Aloha{
 	public static void main(String[] args) {
+		while(true){
 		System.out.println("input name");
+		String name = new java.util.Scanner(System.in).nextLine();
 		System.out.println(name);
-			System.out.println("おわりました");
-			
+		if (name.equals("exit")) {
+		break;
+		}			
 		}
 			
 	

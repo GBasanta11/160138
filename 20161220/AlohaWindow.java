ea9260bdbbd37d4fa7a37674982db649ec5570ad
GBasanta11 <gbasanta37@gmail.com>
2016-12-20 11:39:49 +0900

    AlohaWindow.java Modefied.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -3,7 +3,16 @@ import javax.swing.*;
 public class AlohaWindow{
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(200,200);
+		frame.setSize(400,400);
+		frame.setTitle("ギミレ　バサンタ");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new FlowLayout());
+
+		JLabel label = new JLabel("HELLO!!");
+		Container ctnr = frame.getContentPane();
+		ctnr.add(label);
+		JButton button = new JButton("CLICK!!");
+		ctnr.add(button);
 		frame.setVisible(true);
 	}
 }

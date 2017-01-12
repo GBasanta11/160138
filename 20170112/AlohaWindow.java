d3692bd35592e7810d8867d876f7bfc1ec739247
GBasanta11 <gbasanta37@gmail.com>
2017-01-12 12:06:39 +0900

    window added

---
+++ 20170112/AlohaWindow.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+public class AlohaWindow{
+	
+public static void main(String[] args) {
+
+JFrame frame;
+Container con;
+JLabel label;
+JButton button;
+
+button = new JButton("Hello");
+label = new JLabel("Aloha");
+
+frame = new JFrame("AlohaWindow");
+con = frame.getContentPane();
+
+	con.setLayout(new GridLayout(2, 1));
+	con.add(label);
+	con.add(button);
+
+	frame.setLocation(400, 400);
+	frame.setSize(300, 300);
+	frame.setVisible(true);
+}	
+}
